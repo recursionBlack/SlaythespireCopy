0bc6c9e250a842a118093bb2af2263d93d433907
Kuan <recursionBlack@163.com>
2026-06-14 17:46:28 +0800

    S2 E03-2、创建卡牌堆视图

---
+++ deck-builder-game/scene/ui/card_pile_view.gd
@@ -0,0 +1,54 @@
+extends Control
+class_name CardPileView
+
+const CARD_MENU_UI_SCENE := preload("res://scene/ui/card_menu_ui.tscn")
+
+@export var card_pile: CardPile
+
+@onready var title: Label = %Title
+@onready var cards: GridContainer = %Cards
+@onready var card_tooltip_popup: CardTooltipPopup = %CardTooltipPopup
+@onready var back_btn: Button = %BackBtn
+
+
+func _ready() -> void:
+	back_btn.pressed.connect(hide)
+	
+	# 清除测试用的卡牌占位符
+	for card:Node in cards.get_children():
+		card.queue_free()
+	
+	card_tooltip_popup.hide_tooltip()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		if card_tooltip_popup.visible:
+			card_tooltip_popup.hide_tooltip()
+		else:
+			hide()
+
+
+func show_current_view(new_title: String, randomized: bool = false) -> void:
+	for card: Node in cards.get_children():
+		card.queue_free()
+	
+	card_tooltip_popup.hide_tooltip()
+	_update_view.call_deferred(randomized)
+
+
+func _update_view(randomized: bool) -> void:
+	if not card_pile:
+		return
+	
+	var all_cards := card_pile.cards.duplicate()
+	if randomized:
+		all_cards.shuffle()
+	
+	for card: Card in all_cards:
+		var new_card := CARD_MENU_UI_SCENE.instantiate() as CardMenuUI
+		cards.add_child(new_card)
+		new_card.card = card
+		new_card.tooltip_requested.connect(card_tooltip_popup.show_tooltip)
+	
+	show()
