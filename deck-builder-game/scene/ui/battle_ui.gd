97bda1c5ba520e44bb94ea75774f26dae6aa327f
Kuan <recursionBlack@163.com>
2026-06-14 20:07:09 +0800

    S2 E03-3、卡牌堆视图交互

--- deck-builder-game/scene/ui/battle_ui.gd
+++ deck-builder-game/scene/ui/battle_ui.gd
@@ -7,11 +7,25 @@ class_name BattleUI
 @onready var hand: Hand = $Hand
 @onready var mana_ui: ManaUI = $ManaUI as ManaUI
 @onready var end_turn_btn: Button = %EndTurnBtn
+@onready var draw_pile_btn: CardPileOpener = %DrawPileBtn
+@onready var discard_pile_btn: CardPileOpener = %DiscardPileBtn
+@onready var draw_pile_view: CardPileView = %DrawPileView
+@onready var discard_pile_view: CardPileView = %DiscardPileView
 
 
 func _ready() -> void:
 	Events.player_hand_drawn.connect(_on_player_hand_drawn)
 	end_turn_btn.pressed.connect(_on_end_turn_button_pressed)
+	draw_pile_btn.pressed.connect(draw_pile_view.show_current_view.bind("Draw Pile", true))
+	discard_pile_btn.pressed.connect(discard_pile_view.show_current_view.bind("Discard Pile"))
+
+
+func initialize_card_pile_ui() -> void:
+	draw_pile_btn.card_pile = char_stats.draw_pile
+	draw_pile_view.card_pile = char_stats.draw_pile
+	discard_pile_btn.card_pile = char_stats.discard
+	discard_pile_view.card_pile = char_stats.discard
+
 
 func _set_char_stats(value: CharacterStats) -> void:
 	char_stats = value
