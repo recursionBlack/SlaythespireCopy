600b0624087bd97ecc41439162e88794f4b0ec05
Kuan <recursionBlack@163.com>
2026-06-15 17:21:13 +0800

    S2 E05-1、地图生成介绍与算法

---
+++ deck-builder-game/custom_resources/room.gd
@@ -0,0 +1,16 @@
+class_name Room
+extends Resource
+
+enum	 Type {NOT_ASSIGNED, MONSTER, TREASURE, CAMPFIRE, SHOP, BOSS}
+
+@export var type: Type
+@export var row: int
+@export var column: int
+@export var position: Vector2
+@export var next_rooms: Array[Room]
+@export var selected := false
+
+
+func _to_string() -> String:
+	# 打印出房间类型的首字母
+	return "%s (%s)" % [column, Type.keys()[type][0]]
---
+++ deck-builder-game/scene/map/map_generator.gd
@@ -0,0 +1,19 @@
+class_name MapGenerator
+extends Node
+
+const X_DIST := 30
+const Y_DIST := 25
+const PLACEMENT_RANDOMNESS := 5
+const FLOORS := 15
+const MAP_WIDTH := 7
+const PATHS := 6
+const MONSTER_ROOM_WEIGHT := 10.0
+const SHOP_ROOM_WEIGHT := 2.5
+const CAMPFIRE_ROOM_WEIGHT := 4.0
+
+
+var random_room_type_weights = {
+	Room.Type.MONSTER: 0.0,
+	Room.Type.CAMPFIRE: 0.0,
+	Room.Type.SHOP: 0.0,
+}
