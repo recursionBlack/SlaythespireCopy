e9cc84ee168aa034447d233067bcd56d5719e870
Kuan <recursionBlack@163.com>
2026-06-16 20:16:28 +0800

    S2 E08-1、状态系统架构与资源创建

---
+++ deck-builder-game/custom_resources/status.gd
@@ -0,0 +1,44 @@
+class_name Status
+extends Resource
+
+signal status_applied(status: Status)
+signal status_changed
+
+enum Type {STATE_OF_TURN, END_OF_TURN, EVENT_BASED}
+enum StackType {NONE, INTENSITY, DURATION}
+
+@export_group("Status Data")
+@export var id: String
+@export var type: Type
+@export var stack_type: StackType
+@export var can_expire: bool
+@export var duration: int: set = set_duration
+@export var stacks: int: set = set_stacks
+
+@export_group("Status visual")
+@export var icon: Texture
+@export_multiline var tooltip: String
+
+
+func initiate_status(_target: Node) -> void:
+	pass
+
+
+func apply_status(_target: Node) -> void:
+	status_applied.emit(self)
+
+
+func get_tooltip() -> String:
+	return tooltip
+
+
+# 持续回合
+func set_duration(new_duration: int) -> void:
+	duration = new_duration
+	status_changed.emit()
+
+
+# 叠加层数
+func set_stacks(new_stacks: int) -> void:
+	stacks = new_stacks
+	status_changed.emit()
