da315860103d1d60942ceed8fd63923e8d2d9d04
Kuan <recursionBlack@163.com>
2026-06-19 16:20:35 +0800

    S2 E12-2、商店与宝箱房：商店遗物实现与商店场景完成

--- deck-builder-game/scene/shop/shop.gd
+++ deck-builder-game/scene/shop/shop.gd
@@ -1,13 +1,29 @@
 class_name Shop
 extends Control
 
+const SHOP_CARD = preload("res://scene/shop/shop_card.tscn")
+const SHOP_RELIC = preload("res://scene/shop/shop_relic.tscn")
+
+@export var shop_relics: Array[Relic]
+@export var char_stats: CharacterStats
+@export var run_stats: RunStats
+@export var relic_handler: RelicHandler
+
 @onready var cards: HBoxContainer = %Cards
+@onready var relics: HBoxContainer = %Relics
 @onready var card_tooltip_popup: CardTooltipPopup = %CardTooltipPopup
 
 
 func _ready() -> void:
+	# 先删除开发时测试用的临时占位元素
 	for shop_card: ShopCard in cards.get_children():
-		shop_card.current_card_ui.tooltip_requested.connect(card_tooltip_popup.show_tooltip)
+		shop_card.queue_free()
+		
+	for shop_relic: ShopRelic in relics.get_children():
+		shop_relic.queue_free()
+	
+	Events.shop_card_bought.connect(_on_shop_card_bought)
+	Events.shop_relic_bought.connect(_on_shop_relic_bought)
 
 
 func _input(event: InputEvent) -> void:
@@ -15,5 +31,66 @@ func _input(event: InputEvent) -> void:
 		card_tooltip_popup.hide_tooltip()
 
 
+func populate_shop() -> void:
+	_generate_shop_cards()
+	_generate_shop_relics()
+
+
+func _generate_shop_cards() -> void:
+	var shop_card_array: Array[Card] = []
+	var available_cards := char_stats.draftable_cards.cards.duplicate()
+	available_cards.shuffle()
+	shop_card_array = available_cards.slice(0, 3)
+	
+	for card: Card in shop_card_array:
+		var new_shop_card := SHOP_CARD.instantiate() as ShopCard
+		cards.add_child(new_shop_card)
+		new_shop_card.card = card
+		new_shop_card.current_card_ui.tooltip_requested.connect(card_tooltip_popup.show_tooltip)
+		new_shop_card.update(run_stats)
+
+
+func _generate_shop_relics() -> void:
+	var shop_relic_array: Array[Relic] = []
+	var available_relics := shop_relics.filter(
+		func(relic: Relic):
+			var can_appear := relic.can_appear_as_reward(	char_stats)
+			var already_had_it := relic_handler.has_relic(relic.id)
+			
+			return can_appear and not already_had_it
+	)
+	
+	available_relics.shuffle()
+	shop_relic_array = available_relics.slice(0, 3)
+	
+	for relic: Relic in shop_relic_array:
+		var new_shop_relic := SHOP_RELIC.instantiate() as ShopRelic
+		relics.add_child(new_shop_relic)
+		new_shop_relic.relic = relic
+		new_shop_relic.update(run_stats)
+
+
+func _update_items() -> void:
+	for shop_card: ShopCard in cards.get_children():
+		shop_card.update(run_stats)
+	
+	for shop_relic: ShopRelic in relics.get_children():
+		shop_relic.update(run_stats)
+
+
 func _on_back_btn_pressed() -> void:
 	Events.shop_exited.emit()
+
+
+func _on_shop_card_bought(card: Card, gold_cost: int) -> void:
+	char_stats.deck.add_card(card)
+	run_stats.gold -= gold_cost
+	
+	_update_items()
+
+
+func _on_shop_relic_bought(relic: Relic, gold_cost: int) -> void:
+	relic_handler.add_relic(relic)
+	run_stats.gold -= gold_cost
+	
+	_update_items()
